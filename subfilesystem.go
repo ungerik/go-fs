8cb79d3d80ffde5e1f37f943e935c33db22a5067
Erik Unger <erik@erikunger.com>
2017-01-06 21:20:33 +0100

    SubFileSystem

---
+++ subfilesystem.go
@@ -0,0 +1,34 @@
+package fs
+
+import (
+	"path/filepath"
+	"strings"
+)
+
+type SubFileSystem struct {
+	Parent   FileSystem
+	BasePath string
+}
+
+func (fs *SubFileSystem) IsReadOnly() bool {
+	return fs.Parent.IsReadOnly()
+}
+
+func (fs *SubFileSystem) Prefix() string {
+	return fs.Parent.Prefix()
+}
+
+func (fs *SubFileSystem) Name() string {
+	return "Sub file system of " + fs.Parent.Name()
+}
+
+func (fs *SubFileSystem) File(subPath string) File {
+	if strings.HasPrefix(subPath, fs.Prefix()) || strings.HasPrefix(subPath, "..") || strings.ContainsRune(subPath, ':') {
+		panic("invalid subPath for SubFileSystem: " + subPath)
+	}
+	cleanedPath := filepath.Clean(subPath)
+	if cleanedPath == "/" || strings.ContainsRune(cleanedPath, ':') {
+		panic("invalid subPath for SubFileSystem: " + subPath)
+	}
+	return fs.Parent.File(filepath.Join(fs.BasePath, cleanedPath))
+}
