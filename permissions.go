f8ad3b69da74fff430566bb35d45e392545b5dbe
Erik Unger <erik@erikunger.com>
2017-01-06 22:44:09 +0100

    more Permisssions methods

--- permissions.go
+++ permissions.go
@@ -4,20 +4,20 @@ var (
 	UserExecute   Permissions = 0100
 	UserWrite     Permissions = 0200
 	UserRead      Permissions = 0400
-	UserReadWrite             = UserRead + UserWrite
+	UserReadWrite Permissions = UserRead + UserWrite
 
 	GroupExecute   Permissions = 0010
 	GroupWrite     Permissions = 0020
 	GroupRead      Permissions = 0040
-	GroupReadWrite             = GroupRead + GroupWrite
+	GroupReadWrite Permissions = GroupRead + GroupWrite
 
-	UserGroupRead      = UserRead + GroupRead
-	UserGroupReadWrite = UserReadWrite + GroupReadWrite
+	UserAndGroupRead      Permissions = UserRead + GroupRead
+	UserAndGroupReadWrite Permissions = UserReadWrite + GroupReadWrite
 
 	OthersExecute   Permissions = 0001
 	OthersWrite     Permissions = 0002
 	OthersRead      Permissions = 0004
-	OthersReadWrite             = OthersRead + OthersWrite
+	OthersReadWrite Permissions = OthersRead + OthersWrite
 
 	AllRead      = UserRead + GroupRead + OthersRead
 	AllWrite     = UserWrite + GroupWrite + OthersWrite
@@ -26,10 +26,6 @@ var (
 )
 
 type Permissions int
-
-func (perm Permissions) Can(p Permissions) bool {
-	return perm&p != 0
-}
 
 func (perm Permissions) Readable() (user, group, others bool) {
 	return perm&UserRead != 0, perm&GroupRead != 0, perm&OthersRead != 0
@@ -42,6 +38,33 @@ func (perm Permissions) Writable() (user, group, others bool) {
 func (perm Permissions) Executable() (user, group, others bool) {
 	return perm&UserExecute != 0, perm&GroupExecute != 0, perm&OthersExecute != 0
 }
+
+func (perm Permissions) Can(p Permissions) bool {
+	return perm&p == p
+}
+
+func (perm Permissions) CanUserExecute() bool   { return perm.Can(UserExecute) }
+func (perm Permissions) CanUserWrite() bool     { return perm.Can(UserWrite) }
+func (perm Permissions) CanUserRead() bool      { return perm.Can(UserRead) }
+func (perm Permissions) CanUserReadWrite() bool { return perm.Can(UserReadWrite) }
+
+func (perm Permissions) CanGroupExecute() bool   { return perm.Can(GroupExecute) }
+func (perm Permissions) CanGroupWrite() bool     { return perm.Can(GroupWrite) }
+func (perm Permissions) CanGroupRead() bool      { return perm.Can(GroupRead) }
+func (perm Permissions) CanGroupReadWrite() bool { return perm.Can(GroupReadWrite) }
+
+func (perm Permissions) CanUserAndGroupRead() bool      { return perm.Can(UserAndGroupRead) }
+func (perm Permissions) CanUserAndGroupReadWrite() bool { return perm.Can(UserAndGroupReadWrite) }
+
+func (perm Permissions) CanOthersExecute() bool   { return perm.Can(OthersExecute) }
+func (perm Permissions) CanOthersWrite() bool     { return perm.Can(OthersWrite) }
+func (perm Permissions) CanOthersRead() bool      { return perm.Can(OthersRead) }
+func (perm Permissions) CanOthersReadWrite() bool { return perm.Can(OthersReadWrite) }
+
+func (perm Permissions) CanAllRead() bool      { return perm.Can(AllRead) }
+func (perm Permissions) CanAllWrite() bool     { return perm.Can(AllWrite) }
+func (perm Permissions) CanAllExecute() bool   { return perm.Can(AllExecute) }
+func (perm Permissions) CanAllReadWrite() bool { return perm.Can(AllReadWrite) }
 
 func CombinePermissions(perms []Permissions, defaultPerm Permissions) (result Permissions) {
 	if len(perms) == 0 {
