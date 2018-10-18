678462b2a65373a35b6cb09a3cfc54a6b24347e8
Erik Unger <erik@erikunger.com>
2018-10-18 17:58:41 +0200

    added ServeHTTP

--- httphandler.go
+++ httphandler.go
@@ -10,23 +10,31 @@ import (
 // error while reading it.
 func HTTPHandler(file FileReader, contentType ...string) http.Handler {
 	return http.HandlerFunc(func(response http.ResponseWriter, request *http.Request) {
-		data, err := file.ReadAll()
-		if err != nil {
-			statusCode := http.StatusInternalServerError
-			if IsErrDoesNotExist(err) {
-				statusCode = http.StatusNotFound
-			}
-			http.Error(response, http.StatusText(statusCode), statusCode)
-			return
-		}
-
-		if len(contentType) == 0 {
-			response.Header().Add("Content-Type", http.DetectContentType(data))
-		} else {
-			for _, ct := range contentType {
-				response.Header().Add("Content-Type", ct)
-			}
-		}
-		response.Write(data)
+		ServeHTTP(response, request, file, contentType...)
 	})
+}
+
+// ServeHTTP serves the passed file with a Content-Type header.
+// If no contentType is passed then http.DetectContentType is used with the file content.
+// 404 error is returned if the file does not exist and a 500 error if there was any other
+// error while reading it.
+func ServeHTTP(response http.ResponseWriter, request *http.Request, file FileReader, contentType ...string) {
+	data, err := file.ReadAll()
+	if err != nil {
+		statusCode := http.StatusInternalServerError
+		if IsErrDoesNotExist(err) {
+			statusCode = http.StatusNotFound
+		}
+		http.Error(response, http.StatusText(statusCode), statusCode)
+		return
+	}
+
+	if len(contentType) == 0 {
+		response.Header().Add("Content-Type", http.DetectContentType(data))
+	} else {
+		for _, ct := range contentType {
+			response.Header().Add("Content-Type", ct)
+		}
+	}
+	response.Write(data)
 }
