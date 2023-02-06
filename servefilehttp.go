dc174c94a8e8b4c9fb518b6e5a6e5f89ee3bce84
Erik Unger <erik@erikunger.com>
2023-02-06 15:01:55 +0100

    ServeFileHTTP uses http.ServeContent

--- servefilehttp.go
+++ servefilehttp.go
@@ -1,16 +1,18 @@
 package fs
 
 import (
-	"context"
 	"errors"
 	"net/http"
-	"strconv"
+	"os"
+	"time"
 )
 
-// ServeFileHTTPHandler returns a http.Handler that serves the passed file with a Content-Type header.
-// If no contentType is passed then http.DetectContentType is used with the file content.
-// 404 error is returned if the file does not exist and a 500 error if there was any other
-// error while reading it.
+// ServeFileHTTPHandler returns a http.Handler that serves the passed file with a Content-Type header via HTTP.
+// If no contentType is passed then it will be deduced from the filename and if that fails from the content.
+// A status code 404 error is returned if the file does not exist
+// and a status code 500 error if there was any other error while reading it.
+//
+// Uses http.ServeContent under the hood.
 func ServeFileHTTPHandler(file FileReader, contentType ...string) http.Handler {
 	return http.HandlerFunc(func(response http.ResponseWriter, request *http.Request) {
 		ServeFileHTTP(response, request, file, contentType...)
@@ -18,32 +20,29 @@ func ServeFileHTTPHandler(file FileReader, contentType ...string) http.Handler {
 }
 
 // ServeFileHTTP serves the passed file with a Content-Type header via HTTP.
-// If no contentType is passed then http.DetectContentType is used with the file content.
+// If no contentType is passed then it will be deduced from the filename and if that fails from the content.
 // A status code 404 error is returned if the file does not exist
 // and a status code 500 error if there was any other error while reading it.
+//
+// Uses http.ServeContent under the hood.
 func ServeFileHTTP(response http.ResponseWriter, request *http.Request, file FileReader, contentType ...string) {
-	data, err := file.ReadAllContext(request.Context())
+	readSeeker, err := file.OpenReadSeeker()
 	if err != nil {
-		// ErrDoesNotExist implements http.Handler with a 404 response
-		var errHandler http.Handler
-		if errors.As(err, &errHandler) {
-			errHandler.ServeHTTP(response, request)
-			return
-		}
-
-		if !errors.Is(err, context.Canceled) {
+		if errors.Is(err, os.ErrNotExist) {
+			http.NotFound(response, request)
+		} else {
 			http.Error(response, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 		}
 		return
 	}
+	defer readSeeker.Close()
 
-	response.Header().Set("Content-Length", strconv.Itoa(len(data)))
-	if len(contentType) == 0 {
-		response.Header().Add("Content-Type", http.DetectContentType(data))
-	} else {
-		for _, t := range contentType {
-			response.Header().Add("Content-Type", t)
-		}
+	for _, t := range contentType {
+		response.Header().Add("Content-Type", t)
 	}
-	response.Write(data) //#nosec G104
+	var modTime time.Time
+	if f, ok := file.(File); ok {
+		modTime = f.ModTime()
+	}
+	http.ServeContent(response, request, file.Name(), modTime, readSeeker)
 }
